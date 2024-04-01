1b19e39d02e0c1119f31223ce48b10ae847b77a1
Lesther Ruiz <lruiz0312@hotmail.com>
2024-04-01 16:38:29 -0400

    cleaning code

--- src/functions/TimeWithTimeZone.tsx
+++ src/functions/TimeWithTimeZone.tsx
@@ -7,24 +7,21 @@ function convertirAFechaUTC(date: Date): Date {
 }
 
 function aplicarZonaHoraria(date: Date, timezoneOffset: string): string {
-  console.log("aqui", date.getTimezoneOffset());
+
   const [hours, minutes = "0"] = timezoneOffset.split(":");
   const totalOffset = parseInt(hours) * 60 + parseInt(minutes);
-  console.log("totalOffset", totalOffset);
-  // Aplicar el desfase al tiempo UTC
-  const adjustedDate = new Date(date.getTime() + totalOffset * 60000);
-  console.log("adjustedDate", adjustedDate);
-  console.log("adjustedDate UTC", adjustedDate.getUTCDate());
-  console.log("date UTC", date.getUTCDate());
+  
 
-  // Comparar las fechas para ver si cruzó al día siguiente
+  const adjustedDate = new Date(date.getTime() + totalOffset * 60000);
+
+
   const dayIndicator =
-    adjustedDate.getUTCDate() < date.getUTCDate() ? "" : "+1";
+    adjustedDate.getUTCDate() <= date.getUTCDate() ? "" : "+1";
 
   return adjustedDate.toISOString().substring(11, 16) + dayIndicator;
 }
 
 export const TimeWithTimeZone = (date: Date, timezoneOffset: string) => {
-  // const dateUTC = convertirAFechaUTC(date);
+
   return aplicarZonaHoraria(date, timezoneOffset);
 };
